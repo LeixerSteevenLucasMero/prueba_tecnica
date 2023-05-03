d392e17f3398e8643cfaa3bff6f0d525448766ea
LeixerSteevenLucasMero <79165244+LeixerSteevenLucasMero@users.noreply.github.com>
2023-05-03 08:59:37 -0500

    paso6:Imprimir los números primos del vector y la posición

--- ejercicio3/Program.cs
+++ ejercicio3/Program.cs
@@ -148,4 +148,18 @@ namespace ejercicio3
             return suma;
         }
 
+        //paso6:Imprimir los números primos del vector y la posición
+
+        static bool EsPrimo(int numero)
+        {
+            if (numero < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 }
