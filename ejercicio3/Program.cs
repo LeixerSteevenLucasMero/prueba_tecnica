c5166590c49d084ba827397ca1d00079d5a97c0f
LeixerSteevenLucasMero <79165244+LeixerSteevenLucasMero@users.noreply.github.com>
2023-05-03 08:20:44 -0500

    paso1: Generar un menú

---
+++ ejercicio3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        const int VECTOR_SIZE = 50;
+        const int MATRIX_SIZE = 5;
+
+        static void Main(string[] args)
+        {
+            //paso1: Generar un menú
+            int opcion;
+            do
+            {
+                Console.WriteLine("1. Mostrar vector");
+                Console.WriteLine("2. Mostrar matriz");
+                Console.WriteLine("3. Ordenar vector");
+                Console.WriteLine("4. Suma total vector");
+                Console.WriteLine("5. Suma total matriz");
+                Console.WriteLine("6. Imprimir números primos del vector");
+                Console.WriteLine("7. Imprimir números impares de la matriz");
+                Console.WriteLine("8. Ordenar matriz");
+                Console.WriteLine("9. Salir");
+                Console.Write("Ingrese una opción: ");
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+    }
+}
